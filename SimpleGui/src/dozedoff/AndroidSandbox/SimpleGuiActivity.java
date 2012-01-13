eb37e57171696800044dae96583c9cd11c3eb0d3
dozedoff <dozedoffagain@gmail.com>
2012-01-14 00:54:44 +0100

    Toasts working

--- SimpleGui/src/dozedoff/AndroidSandbox/SimpleGuiActivity.java
+++ SimpleGui/src/dozedoff/AndroidSandbox/SimpleGuiActivity.java
@@ -3,13 +3,16 @@ package dozedoff.AndroidSandbox;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SimpleGuiActivity extends Activity {
-	TextView text;
-	int counter = 0;
-	Button button;
+	private TextView text;
+	private int counter = 0;
+	private Button button;
+	private Button btnToast;
 	
 	
     /** Called when the activity is first created. */
@@ -20,13 +23,22 @@ public class SimpleGuiActivity extends Activity {
         
         text = (TextView)this.findViewById(R.id.text);
         button = (Button)this.findViewById(R.id.button);
+        btnToast = (Button)this.findViewById(R.id.btnToast);
         
         updateText();
-        button.setOnClickListener(new Button.OnClickListener() {
+        button.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View v) {
 				counter++;
 				updateText();
+			}
+		});
+        
+        btnToast.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+			Toast toast = Toast.makeText(getApplicationContext(), "A Toast to you!", 1);
+			toast.show();
 			}
 		});
     }
