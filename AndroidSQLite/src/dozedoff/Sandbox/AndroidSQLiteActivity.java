88ab630b0d5ba4a5de19f3141d61466c02d6799f
dozedoff <dozedoffagain@gmail.com>
2012-01-14 21:24:41 +0100

    database read code done

--- AndroidSQLite/src/dozedoff/Sandbox/AndroidSQLiteActivity.java
+++ AndroidSQLite/src/dozedoff/Sandbox/AndroidSQLiteActivity.java
@@ -1,7 +1,9 @@
 package dozedoff.Sandbox;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,6 +14,8 @@ public class AndroidSQLiteActivity extends Activity {
 	SQLiteDatabase db;
 	EditText txtInput;
 	TableLayout table;
+	final String TABLE_NAME = "test";
+	final String[] TABLE_TO_READ = {"simplestring"};
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,16 +28,43 @@ public class AndroidSQLiteActivity extends Activity {
         txtInput = (EditText)this.findViewById(R.id.txtInput);
 		table = (TableLayout)this.findViewById(R.id.table);
 		
+		loadFromDb(db);
+		
 		txtInput.setOnEditorActionListener(new OnEditorActionListener() {
 
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 				if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+					TextView newtext = new TextView(getApplicationContext());
+					newtext.setText(txtInput.getText());
+					table.addView(newtext);
 					
+					if(db.insert(TABLE_NAME, "", addText(txtInput.getText().toString())) == -1){
+						Toast toast = Toast.makeText(getApplicationContext(), "Database write failed", Toast.LENGTH_SHORT);
+						toast.show();
+					}
+					
+					txtInput.setText("");
 					return true;
 				}
 				return false;
 			}
 		});
+    }
+    
+    private ContentValues addText(String text){
+    	ContentValues content = new ContentValues();
+    	content.put("simplestring", text);
+    	return content;
+    }
+    
+    private void loadFromDb(SQLiteDatabase db){
+    	Cursor cursor = db.query(TABLE_NAME, TABLE_TO_READ, null, null, null, null, null);
+    	
+    	while(cursor.moveToNext()){
+    		TextView newtext = new TextView(getApplicationContext());
+			newtext.setText(cursor.getString(0));
+			table.addView(newtext);
+    	}
     }
     
     @Override
