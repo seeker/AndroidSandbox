3df42043498bdd8e85f00435468e188b96511e50
dozedoff <dozedoffagain@gmail.com>
2012-01-14 14:41:20 +0100

    added EditText (a Textfield) and response to enter key

--- SimpleGui/src/dozedoff/AndroidSandbox/SimpleGuiActivity.java
+++ SimpleGui/src/dozedoff/AndroidSandbox/SimpleGuiActivity.java
@@ -2,10 +2,13 @@ package dozedoff.AndroidSandbox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 public class SimpleGuiActivity extends Activity {
@@ -13,7 +16,7 @@ public class SimpleGuiActivity extends Activity {
 	private int counter = 0;
 	private Button button;
 	private Button btnToast;
-	
+	private EditText txtInput;
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,6 +27,7 @@ public class SimpleGuiActivity extends Activity {
         text = (TextView)this.findViewById(R.id.text);
         button = (Button)this.findViewById(R.id.button);
         btnToast = (Button)this.findViewById(R.id.btnToast);
+        txtInput = (EditText)this.findViewById(R.id.txtInput);
         
         updateText();
         button.setOnClickListener(new OnClickListener() {
@@ -39,6 +43,18 @@ public class SimpleGuiActivity extends Activity {
 			public void onClick(View v) {
 			Toast toast = Toast.makeText(getApplicationContext(), "A Toast to you!", 1);
 			toast.show();
+			}
+		});
+        
+        txtInput.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+					Toast toast = Toast.makeText(getApplicationContext(), "You entered the text: "+txtInput.getText(), 1);
+					toast.show();
+					return true;
+				}
+				return false;
 			}
 		});
     }
