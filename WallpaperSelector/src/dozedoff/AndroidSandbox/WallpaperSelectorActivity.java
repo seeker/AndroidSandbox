c27f8dc0048c18e52647fdb98a0b279ffab8e2a9
dozedoff <dozedoffagain@gmail.com>
2012-01-15 18:50:27 +0100

    code should work, however app is missing rights to set wallpaper

--- WallpaperSelector/src/dozedoff/AndroidSandbox/WallpaperSelectorActivity.java
+++ WallpaperSelector/src/dozedoff/AndroidSandbox/WallpaperSelectorActivity.java
@@ -1,9 +1,19 @@
 package dozedoff.AndroidSandbox;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import android.app.Activity;
+import android.app.WallpaperManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,7 +33,7 @@ public class WallpaperSelectorActivity extends Activity {
 			
 			public void onClick(View v) {
 				Intent intent = new Intent("org.openintents.action.PICK_FILE");
-				intent.setData(Uri.parse("file:///sdcard/"));
+				intent.setData(Uri.parse("file:///sdcard/Images/"));
 				intent.putExtra("org.openintents.extra.TITLE", "Please select a file");
 				startActivityForResult(intent, 1);
 				}
@@ -32,13 +42,30 @@ public class WallpaperSelectorActivity extends Activity {
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	if(requestCode == RESULT_OK){
-    		Toast toast = Toast.makeText(getApplicationContext(), data.getData().toString(), Toast.LENGTH_SHORT);
-    		toast.show();
-    		super.onActivityResult(requestCode, resultCode, data);
+    	if(resultCode == RESULT_OK){
+    		String path = data.getDataString().toString();
+
+    		if(path.toLowerCase().endsWith(".jpg") || path.toLowerCase().endsWith(".png")){
+    			InputStream in = null;
+    			try{
+    				
+    				in = new BufferedInputStream(new FileInputStream(new File(new URI(data.getData().toString()))));
+    				WallpaperManager.getInstance(getApplicationContext()).setStream(in);
+    				in.close();
+    			}catch(IOException ioe){
+    				Toast.makeText(getApplicationContext(), "failed to set wallpaper: "+ioe.getMessage(), Toast.LENGTH_LONG).show();
+    				Log.e("Set WP", ioe.getMessage());
+    			} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}else{
+    			Toast.makeText(getApplicationContext(), "Not an image", Toast.LENGTH_SHORT).show();
+    		}
     	}else{
     		Toast toast = Toast.makeText(getApplicationContext(), "No file selected", Toast.LENGTH_SHORT);
     		toast.show();
     	}
+		super.onActivityResult(requestCode, resultCode, data);
     }
 }
