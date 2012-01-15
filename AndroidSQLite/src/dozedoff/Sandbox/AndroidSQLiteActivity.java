4914605d950658fab673f6494d444d90da7f10c9
dozedoff <dozedoffagain@gmail.com>
2012-01-15 03:53:38 +0100

    ListFragment finally works. Faulty XML layout.

--- AndroidSQLite/src/dozedoff/Sandbox/AndroidSQLiteActivity.java
+++ AndroidSQLite/src/dozedoff/Sandbox/AndroidSQLiteActivity.java
@@ -1,23 +1,34 @@
 package dozedoff.Sandbox;
 
-import android.app.Activity;
 import android.content.ContentValues;
-import android.content.res.Configuration;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ListFragment;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.KeyEvent;
-import android.widget.*;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
-public class AndroidSQLiteActivity extends Activity {
+public class AndroidSQLiteActivity extends FragmentActivity {
 	SQLiteDatabase db;
 	EditText txtInput;
-	TableLayout table;
+	SimpleCursorAdapter simpleCursorAd;
+
 	final String TABLE_NAME = "test";
-	final String[] TABLE_TO_READ = {"simplestring"};
-	
-    /** Called when the activity is first created. */
+	final String[] COLUMN_TO_READ = {"simplestring"};
+	final String[] COLUMNS_FOR_ADAPTER = {"_id","simplestring"};
+	final String RAW_QUERY = "SELECT oid as _id, simplestring FROM "+TABLE_NAME;
+	final int[] to = {R.id.rowid,R.id.simplestring};
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +37,6 @@ public class AndroidSQLiteActivity extends Activity {
         db = dbHelper.getWritableDatabase();
         
         txtInput = (EditText)this.findViewById(R.id.txtInput);
-		table = (TableLayout)this.findViewById(R.id.table);
 		
 		loadFromDb(db);
 		
@@ -34,15 +44,11 @@ public class AndroidSQLiteActivity extends Activity {
 
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 				if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
-					TextView newtext = new TextView(getApplicationContext());
-					newtext.setText(txtInput.getText());
-					table.addView(newtext);
-					
 					if(db.insert(TABLE_NAME, "", addText(txtInput.getText().toString())) == -1){
 						Toast toast = Toast.makeText(getApplicationContext(), "Database write failed", Toast.LENGTH_SHORT);
 						toast.show();
 					}
-					
+					refreshList(db);
 					txtInput.setText("");
 					return true;
 				}
@@ -66,17 +72,16 @@ public class AndroidSQLiteActivity extends Activity {
     }
     
     private void loadFromDb(SQLiteDatabase db){
-    	Cursor cursor = db.query(TABLE_NAME, TABLE_TO_READ, null, null, null, null, null);
-    	
-    	while(cursor.moveToNext()){
-    		TextView newtext = new TextView(getApplicationContext());
-			newtext.setText(cursor.getString(0));
-			table.addView(newtext);
-    	}
+    	Cursor cursor = db.rawQuery(RAW_QUERY, null);
+    	    	
+    	simpleCursorAd = new SimpleCursorAdapter(getApplicationContext(), R.layout.listlayout, cursor, COLUMNS_FOR_ADAPTER, to, SimpleCursorAdapter.NO_SELECTION);
+    	FragmentManager fm = getSupportFragmentManager();
+    	ListFragment frag = (ListFragment)fm.findFragmentById(R.id.list);
+    	frag.setListAdapter(simpleCursorAd);
     }
     
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
+    private void refreshList(SQLiteDatabase db){
+    	Cursor cursor = db.rawQuery(RAW_QUERY, null);
+    	simpleCursorAd.changeCursor(cursor);
     }
 }
